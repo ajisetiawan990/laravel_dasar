5a7d5dc9f958c615e9840183fecb92c5cfc0a20c
ajisetiawan990 <ajidec10@gmail.com>
2025-08-12 11:48:35 +0700

    first commit

--- database/migrations/2025_08_12_013052_create_products_table.php
+++ database/migrations/2025_08_12_013052_create_products_table.php
@@ -13,10 +13,10 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('category_id')->constrained()->onDelete('cascade');
             $table->string('name');
             $table->integer('price');
             $table->integer('stock');
+            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
             $table->timestamps();
         });
     }
